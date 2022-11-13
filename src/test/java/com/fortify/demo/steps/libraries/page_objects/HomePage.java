86ab1966516b957ea7aefcc5de209a1a641530ac
Martin Cieslar <martinx.cieslar@seznam.cz>
2022-11-13 23:32:16 +0100

    headless measures

--- src/test/java/com/fortify/demo/steps/libraries/page_objects/HomePage.java
+++ src/test/java/com/fortify/demo/steps/libraries/page_objects/HomePage.java
@@ -17,14 +17,17 @@ public class HomePage extends PageObject {
 
         System.out.printf("navigator.userAgent: %s%n", getJavascriptExecutorFacade().executeScript("return navigator.userAgent"));
         System.out.printf("window.navigator.userAgent: %s%n", getJavascriptExecutorFacade().executeScript("return window.navigator.userAgent"));
-        System.out.printf("navigator.webdriver: %s%n", getJavascriptExecutorFacade().executeScript("return navigator.webdriver"));
-        System.out.printf("!window.chrome: %s%n", getJavascriptExecutorFacade().executeScript("return !window.chrome"));
+        System.out.printf("navigator.webdriver, is headless: %s%n",
+                          getJavascriptExecutorFacade().executeScript("return navigator.webdriver"));
+        System.out.printf("!window.chrome, is headless: %s%n", getJavascriptExecutorFacade().executeScript("return !window.chrome"));
         System.out.printf("navigator.permissions, is headless: %s%n",
                           getJavascriptExecutorFacade().executeScript("return navigator.permissions.query({name:'notifications'}).then(function(permissionStatus) {return (Notification.permission === 'denied' && permissionStatus.state === 'prompt');});"));
         System.out.printf("navigator.plugins, can be headless: %s%n",
                           getJavascriptExecutorFacade().executeScript("return navigator.plugins.length === 0"));
         System.out.printf("navigator.languages, is headless: %s%n",
                           getJavascriptExecutorFacade().executeScript("return navigator.languages === \"\""));
+        System.out.printf("navigator.languages, list: %s%n",
+                          getJavascriptExecutorFacade().executeScript("return navigator.languages"));
     }
 
 }
