5894d5517c85398fb9dd019d60fa42eccded1d34
Martin Cieslar <martinx.cieslar@seznam.cz>
2022-11-13 17:51:41 +0100

    use different approach to install chrome

--- scripts/install_chrome.sh
+++ scripts/install_chrome.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 set -ex
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+apt-get update \
+    && apt-get install --no-install-recommends --no-install-suggests -y \
+        unzip \
+        curl \
+        sudo \
+        gnupg \
+    # Download chrome install it
+    && GOOGLE_LINUX_DL=https://dl.google.com/linux \
+    && curl -sL "$GOOGLE_LINUX_DL/linux_signing_key.pub" | apt-key add - \
+    && curl -sL "$GOOGLE_LINUX_DL/direct/google-chrome-stable_current_amd64.deb" > /tmp/chrome.deb \
+    && apt install --no-install-recommends --no-install-suggests -y \
+        /tmp/chrome.deb \
+    && CHROMIUM_FLAGS='--no-sandbox --disable-dev-shm-usage --window-size=1920,1080' \
+    && sed -i '${s/$/'" $CHROMIUM_FLAGS"'/}' /opt/google/chrome/google-chrome \
+    #Celan up
+    && apt-get clean \
+    && rm -rf \
+        /tmp/* \
+        /usr/share/doc/* \
+        /var/cache/* \
+        /var/lib/apt/lists/* \
+        /var/tmp/* \
